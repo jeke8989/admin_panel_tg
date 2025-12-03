1008d7a19a5204df2c0aab7980286d96d90fa236
jeke8989 <144659393+jeke8989@users.noreply.github.com>
2025-12-04 02:17:31 +0300

    Update .gitignore to exclude large files and backups

--- fix_triggers.sql
+++
@@ -1,7 +0,0 @@
-DROP TRIGGER IF EXISTS update_users_updated_at ON users;
-DROP TRIGGER IF EXISTS update_chats_updated_at ON chats;
-DROP TRIGGER IF EXISTS update_messages_updated_at ON messages;
-DROP TRIGGER IF EXISTS update_chat_unread_counts_updated_at ON chat_unread_counts;
-DROP FUNCTION IF EXISTS update_updated_at_column();
-
-
